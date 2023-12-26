90baf457f4fff9e3a3aea77c7fee12d22c490c99
Alejandro Parra <alejandro.parra@iepcdurango.mx>
2023-12-26 15:06:23 -0600

    Cambios mínimos

--- main.js
+++ main.js
@@ -708,6 +708,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         /**
          *  CONTEO DE PROPIETARIOS.
+         *  Se alternan géneros según selección en Mayoría Relativa.
          * */
         let _e__male_p = Array.from(document.querySelectorAll('input[value="male"]:checked')).filter(f => f.dataset.position === 'p')
 
